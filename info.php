b5ea261a7099ea0a8176b88723755f57110f3c8e
Ahmet <leaforewer.net@gmail.com>
2021-01-28 21:08:25 +0100

    Carousel in infopagina toegevoegd

--- info.php
+++ info.php
@@ -3,11 +3,64 @@
     <div class="jumbotron" id="jumbotron1" style="    background: linear-gradient(to right, #ff9900 0%, #ff6666 100%);">
         <h1 class="display-4" id="Jumbotron_title" style="text-align:center;">Chinees Nieuwjaar</h1>
         <hr class="my-4">
-        <h3 id="jumbotron_text" style="text-align:center; font-family: 'Quicksand', sans-serif;">Op deze pagina kunt u allerlei informatie vinden over Chinees Nieuwjaar!</h3>
-        <p class="lead">
-        </p>
-    </div>
+    
+    <!-- Carousel Voedsels -->
+    <div id="carouselExampleDark" class="carousel carousel-dark slide col-8 offset-2" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carouselExampleDark" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carouselExampleDark" data-bs-slide-to="2"></li>
+            <li data-bs-target="#carouselExampleDark" data-bs-slide-to="3"></li>
 
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+                <img src="./img/jiaozi2.jpg" class="img-fluid" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="./img/tangyuan.jpg" class="img-fluid" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./img/niangao.jpg" class="img-fluid" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./img/yusheng.jpg" class="img-fluid" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./img/jiandui.jpg" class="img-fluid" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleDark" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleDark" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </a>
+    </div>
+    </div>
+    <!-- Begin van het accordion infomartie -->
     <div class="row" id="accordion" style="font-family: 'Barlow', sans-serif;">
         <!-- eerste tab -->
         <div class="card col-12" style="background: linear-gradient(to left, #cc6600 0%, #ffff66 130%)">
@@ -118,7 +171,7 @@
         <div class="card col-12" style="background: linear-gradient(to left, #cc6600 0%, #ffff66 130%)">
             <div class="card-header" id="headingFive">
                 <h5 class="mb-0">
-                    <button class="btn collapsed" style="font-size: 20px;" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive" id="card_collapse_text">
+                    <button class="btn collapsed" style="font-size: 20px;" ~ data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive" id="card_collapse_text">
                         In Nederland?
                     </button>
                 </h5>
